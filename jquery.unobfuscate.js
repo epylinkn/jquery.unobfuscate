a367fe3544cfa0b495e27a4cfa8e410c40dc116e
ant <anthonybui@gmail.com>
2012-09-25 21:59:13 -0700

    add unobfuscate js plugin; add index.html test page

---
+++ jquery.unobfuscate.js
@@ -0,0 +1,21 @@
+/*
+ * Unobfuscate jQuery Plugin
+ * Copyright 2012, Anthony Bui
+ * MIT Licensed
+ * http://github.com/epylinkn/jquery.unobfuscate
+ */
+(function($) {
+  "use strict";
+
+  $.fn.unobfuscate = function() {
+    return this.each(function() {
+      var email = $(this).html()
+        .replace('^', '@')
+        .replace('$', '.')
+        .replace(/FAKEDIV/g, '');
+      $(this).attr('href', 'mailto:'+email);
+      $(this).html(email);
+    });
+  };
+
+})( jQuery );
